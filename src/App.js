8c099f9e90c9bf55e46e86b6ce992351a6aea907
Sakibul Islam <120340281+SI2105@users.noreply.github.com>
2024-02-26 16:04:40 +0000

    weather Overview moved to component
    The weather overviews are now handled at component level.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,52 @@
 import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useCallback } from 'react';
+import './assets/App.css';
 import Header from './Header';
 import Overview from './Overview';
 import Hourly from './Hourly';
 import Precipitation from './Precipitation';
 import Wind from './Wind';
 import Humidity from './Humidity';
+import { API_KEY } from './config';
 function App() {
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const fetchData = useCallback(async () =>{
+    try{
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+      );
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json()
+      setWeatherData(data);
+      console.log(data);
+      
+    }
+    catch(error){
+
+    }
+  }, [city])
+
+  const handleInputChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    fetchData();
+  };
+  
   return (
     <>
     <Header/>
-    <Overview/>
+    <Overview weatherData={weatherData} city={city} handleSubmit={handleSubmit} handleInputChange={handleInputChange}/>
     <Hourly/>
     <Precipitation/>
     <Wind/>
     <Humidity />
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div></>
+    
+      </>
     
   );
 }
--- src/Overview.js
+++ src/Overview.js
@@ -1,9 +1,30 @@
-function Overview() {
+function Overview({weatherData, city ,handleSubmit, handleInputChange}) {
     return (
-      <div className="Header">
+      <div className="Overview">
         
 
-        <p>I am the Overview</p>
+        <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Enter city Name' value={city} onChange={handleInputChange}/>
+        <button type='submit'>Get Weather</button>
+    </form>
+    {weatherData ? (
+        <>
+        <h2>{weatherData.name}</h2>
+        <p>Temperature: {weatherData.main.temp} C°</p>
+        <p><img src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt=''></img></p>
+        <p>Feels like: {weatherData.main.feels_like} C°</p>
+        <p>Humidity: {weatherData.main.humidity} %</p>
+        <p>Pressure: {weatherData.main.pressure} hPa</p>
+        <p>Wind Speed: {weatherData.wind.speed} m/s</p>
+        <p>Wind Degree: {weatherData.wind.deg}°</p>
+        <p>Date: {new Date().toDateString()}</p>
+        
+        </>
+      ): (
+        
+        <p>Loading weather data...</p>
+
+      )}
       </div>
     );
   }
