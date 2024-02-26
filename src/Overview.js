469f203d25d9de9fc68ae998bed0296e1eee21d4
Sakibul Islam <120340281+SI2105@users.noreply.github.com>
2024-02-26 21:52:42 +0000

    Overview changed

--- src/Overview.js
+++ src/Overview.js
@@ -2,7 +2,6 @@ import './assets/Overview.css'
 export default function Overview({weatherData, city ,handleSubmit, handleInputChange}) {
     return (
       <div className="Overview">
-        
 
         <form onSubmit={handleSubmit}>
         <input type="text" placeholder='Enter city Name' value={city} onChange={handleInputChange}/>
@@ -10,14 +9,15 @@ export default function Overview({weatherData, city ,handleSubmit, handleInputCh
     </form>
     {weatherData ? (
         <div className='result' >
-          <div>
+          <div className='top'>
             <h2>{weatherData.name}</h2>
-            <p>{weatherData.main.temp} C°</p>
+            <img src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt=''></img>
+        
           </div>
 
-            <div>
-              <img src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt=''></img>
-              <p>{weatherData.weather[0].main}</p>
+            <div className='bottom'>
+              <h2>{weatherData.main.temp} C°</h2>
+              <h2>{weatherData.weather[0].main}</h2>
             </div>
         </div>
       ): (
