a2ae262b778e942198bb25b226922a15f76a3a20
Sakibul Islam <120340281+SI2105@users.noreply.github.com>
2024-02-28 14:12:35 +0000

    Overview.js results section changed
    Results section changed to display information more clearly.

--- src/Overview.js
+++ src/Overview.js
@@ -1,5 +1,6 @@
 import './assets/Overview.css'
 export default function Overview({weatherData, city ,handleSubmit, handleInputChange}) {
+  const regionNamesInEnglish = new Intl.DisplayNames(['en'], { type: 'region' });
     return (
       <div className="Overview">
 
@@ -9,15 +10,20 @@ export default function Overview({weatherData, city ,handleSubmit, handleInputCh
     </form>
     {weatherData ? (
         <div className='result' >
-          <div className='top'>
+          <div className="names">
             <h2>{weatherData.name}</h2>
-            <img src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt=''></img>
-        
+            <h2>{regionNamesInEnglish.of(weatherData.sys.country)}</h2>
+          </div>
+            
+          <div className="icons">
+            <img src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt='' ></img>
           </div>
 
-            <div className='bottom'>
-              <h2>{weatherData.main.temp} C°</h2>
-              <h2>{weatherData.weather[0].main}</h2>
+            <div className="temp">
+              <h2 className='maintemp'>{weatherData.main.temp} C°</h2>
+            </div>
+            <div className="descriptionsect">
+              <h2 className='description'>{weatherData.weather[0].main}</h2>
             </div>
         </div>
       ): (
