756c275f82b2cf9c2408ba874a6728a66f73f6e3
Sakibul Islam <120340281+SI2105@users.noreply.github.com>
2024-03-17 23:13:44 +0000

    App.js updated for routing and new API calls added
    App.js updated with react router and new API calls for fishing points.

--- src/App.js
+++ src/App.js
@@ -8,10 +8,13 @@ import Map from './Map';
 import axios from 'axios';
 
 import { API_KEY } from './config';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 function App() {
   const [city, setCity] = useState('');
   const [weatherData, setWeatherData] = useState(null);
   const [portData, setPortData] = useState(null);
+  const [fishingData, setFishingData] = useState(null); 
+  const [radius, setRadius] = useState(10000);
   const fetchData = useCallback(async () =>{
     try{
       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}` //Replace API_KEY with your API KEY
@@ -46,11 +49,27 @@ function App() {
     console.log(data)
     return data
   }
+  const getFishingPoints = async (radius) => {
+    const response = await axios.get(`https://overpass-api.de/api/interpreter?data=[out:json];nwr[fishing](around:${radius},${weatherData.coord.lat},${weatherData.coord.lon});out center;`);  
+    const data = response.data
 
+    for (var i = 0; i < data.elements.length; i++){
+      if(data.elements[i].center){
+        data.elements[i].lat = data.elements[i].center.lat
+        data.elements[i].lon = data.elements[i].center.lon
+      }
+
+    }
+    setFishingData(data)
+  
+    console.log(data)
+    return data
+  }
   useEffect(()=>{
-    let radius = 100000
+    
     if(weatherData){
       getPorts(radius);
+      getFishingPoints(radius);
     }
   }, [weatherData])
 
@@ -63,14 +82,30 @@ function App() {
     fetchData();
   };
   
+
   return (
+    <BrowserRouter>
     <div className='App'>
-    <Header city={city} handleSubmit={handleSubmit} handleInputChange={handleInputChange} />
-    <Overview weatherData={weatherData} city={city} handleSubmit={handleSubmit} handleInputChange={handleInputChange}/>
-    <Hourly/>
-    <Weekly weatherData={weatherData} city={city}/>
-    <Map weatherData={weatherData} portData={portData}/>
+      <Header city={city} handleSubmit={handleSubmit} handleInputChange={handleInputChange} />
+      
+      <Routes>
+        <Route path="/" element={
+          <>
+           <Overview weatherData={weatherData} city={city} handleSubmit={handleSubmit} handleInputChange={handleInputChange}/>
+           <Hourly/>
+           <Weekly weatherData={weatherData} city={city}/>
+           <Map weatherData={weatherData} portData={portData} fishingData={fishingData}/>
+           </>
+        }/>
+         
+        <Route path='/catch-tracker' element={
+          <Map weatherData={weatherData} portData={portData}/>}
+        />
+        
+      </Routes>
+
     </div>
+    </BrowserRouter>
     
   );
 }
