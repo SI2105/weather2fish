616d06a5468da6430417d2a754c1bcf73a9a3e5a
Sakibul Islam <120340281+SI2105@users.noreply.github.com>
2024-03-17 23:12:47 +0000

    Map.js updated
    Map.js now displays markers for fishing points and uses new POIContainer components to display each POI of a given type.

--- src/Map.js
+++ src/Map.js
@@ -4,10 +4,13 @@ import './assets/Map.css'
 import 'leaflet/dist/leaflet.css'
 import { Icon } from "leaflet";
 
+import POIContainer from "./POIContainer";
+
 function ChangeMap({center}) {
+  // Changes maps view to the center lat and lon pair.
   const map = useMap();
   useEffect(()=>{
-    map.setView(center,12);
+    map.setView(center,12); //Use Effect here ensure the map is centered when center pair or the map changes
 
   }, [center,map])
   
@@ -15,29 +18,42 @@ function ChangeMap({center}) {
 }
 
 
-function Map({weatherData,portData}) {
+function Map({weatherData,portData,fishingData}) {
   
     
-    
+  // Below are the custome marker icons for each type off marker.
+
+  //City Marker
   const customIcon = new Icon({
     iconUrl: require('./assets/marker.png'),
     iconSize: [25,40]
   })
 
+  //Port Marker
   const portIcon = new Icon({
     iconUrl: require('./assets/port-sign.png'),
     iconSize: [40,40]
   })
 
+  //Fishing Spot Marker
+  const fishingIcon = new Icon({
+    iconUrl: require('./assets/fishing.png'),
+    iconSize: [32,32]
+  })
+
     return (
+      
       <div className="map">
-        <MapContainer center={[51.505, -0.09]} zoom={5} scrollWheelZoom={true}>
+        <MapContainer center={[51.505, -0.09]} zoom={5} scrollWheelZoom={true}> 
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+            {/* TileLayer contains the map tiles for OpenStreetmap */}
             
-            {weatherData && weatherData.coord && (<Marker position={[weatherData.coord.lat, weatherData.coord.lon ]} icon={customIcon}>
+            { // Creates Marker if weatherData and correspoding coordinates are available.
+            //Displays Marker on the chosen city with a Popup that displays the temperature.
+            weatherData && weatherData.coord && (<Marker position={[weatherData.coord.lat, weatherData.coord.lon ]} icon={customIcon}>
                 <Popup>
                 Temperature : {weatherData.main.temp} C° <br /> 
                 </Popup>
@@ -56,6 +72,17 @@ function Map({weatherData,portData}) {
             )
             }
 
+            {weatherData && fishingData && fishingData.elements && (
+              fishingData.elements.filter((fishing, i)=> (fishing.tags.name) ).map((fishing, i)=>(
+                  <Marker key = {i} position={[fishing.lat, fishing.lon]} icon={fishingIcon}>
+                <Popup>
+                  {fishing.tags.name} <br /> 
+                </Popup>
+                </Marker>
+              ))
+            )
+            }
+
 
 
             {weatherData && weatherData.coord && <ChangeMap center={[weatherData.coord.lat, weatherData.coord.lon ]}></ChangeMap>}
@@ -63,6 +90,11 @@ function Map({weatherData,portData}) {
             
             
         </MapContainer>
+
+        <POIContainer weatherData={weatherData} poiData={portData}/>
+        
+        <POIContainer weatherData={weatherData} poiData={fishingData}/>
+        
 
       </div>
     );
